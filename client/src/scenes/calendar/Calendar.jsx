322c3e130bad3ee387d18dfcd97e45a2b2104416
CJPD00 <carlosjavierpacheco60@gmail.com>
2024-07-02 10:26:05 -0400

    u30

--- client/src/scenes/calendar/Calendar.jsx
+++ client/src/scenes/calendar/Calendar.jsx
@@ -1,22 +1,16 @@
-import {
-  ScheduleComponent,
-  ViewsDirective,
-  ViewDirective,
-  Day,
-  Week,
-  WorkWeek,
-  Month,
-  Agenda,
-  Inject,
-  Resize,
-  DragAndDrop,
-} from "@syncfusion/ej2-react-schedule";
-import { DatePickerComponent } from "@syncfusion/ej2-react-calendars";
 import Header from "../../components/Header";
+import { useTheme } from "@mui/material";
 
 const Calendar = () => {
+  const theme = useTheme();
+
+  const style = {
+    backgroundColor: theme.palette.background.alt,
+    color: theme.palette.text.primary,
+  };
+
   return (
-    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 rounded-3xl">
+    <div className="p-2 m-2 mt-24 md:m-10 md:p-10 rounded-3xl" style={style}>
       <Header title="Calendario" subtitle="Agenda" />
     </div>
   );
